1e88be7b5d25adb3004555ed6a586587f8ed316f
leeseongchi <kyoti0414@naver.com>
2024-03-13 17:53:24 +0900

    240313 Library_Project

---
+++ src/main/java/com/study/library/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.study.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 기존세팅 따라가지말고 밑에세팅 따라가라
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests() // 요청 인증 절차
+                .antMatchers("/server/**", "/auth/**") // 요청 인증 받지말고 요청주소창 띄워라
+                .permitAll() // 전부 허용해라
+                .anyRequest() // 나머지 요청들은
+                .authenticated(); // 인증 받아야 됨
+    }
+}
---
+++ src/main/java/com/study/library/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.study.library.controller;
+
+import com.study.library.aop.annotation.ParamsPrintAspect;
+import com.study.library.dto.SignupReqDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth") // 공통주소 (상위주소)
+public class AuthController {
+
+    @ParamsPrintAspect
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupReqDto signupReqDto) {
+        return ResponseEntity.ok(null);
+    }
+}
