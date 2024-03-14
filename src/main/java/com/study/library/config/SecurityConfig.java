802013f47339183fc42b40b3aff5439dd0e09e81
leeseongchi <kyoti0414@naver.com>
2024-03-14 17:49:57 +0900

    240314 Library_Project

--- src/main/java/com/study/library/config/SecurityConfig.java
+++ src/main/java/com/study/library/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.study.library.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // 기존세팅 따라가지말고 밑에세팅 따라가라
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
