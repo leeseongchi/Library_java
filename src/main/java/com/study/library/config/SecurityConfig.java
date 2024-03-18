538dea7c679d2fe089846ef4b675976df2f1db3c
leeseongchi <kyoti0414@naver.com>
2024-03-18 17:28:33 +0900

    240318 Library_Project

--- src/main/java/com/study/library/config/SecurityConfig.java
+++ src/main/java/com/study/library/config/SecurityConfig.java
@@ -1,15 +1,31 @@
 package com.study.library.config;
 
+import com.study.library.security.exception.AuthEntryPoint;
+import com.study.library.security.filter.JwtAuthenticationFilter;
+import com.study.library.security.filter.PermitAllFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @EnableWebSecurity // 기존세팅 따라가지말고 밑에세팅 따라가라
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PermitAllFilter permitAllFilter;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private AuthEntryPoint authEntryPoint;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -18,11 +34,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable();
         http.authorizeRequests() // 요청 인증 절차
                 .antMatchers("/server/**", "/auth/**") // 요청 인증 받지말고 요청주소창 띄워라
                 .permitAll() // 전부 허용해라
                 .anyRequest() // 나머지 요청들은
-                .authenticated(); // 인증 받아야 됨
+                .authenticated() // 인증 받아야 됨
+                .and()
+                .addFilterAfter(permitAllFilter, LogoutFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint);
     }
 }
