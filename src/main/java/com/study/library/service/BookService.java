6768859a72c214de89236ca7099f1229136cfd4d
leeseongchi <kyoti0414@naver.com>
2024-03-28 17:51:47 +0900

    240328 Library_Project

--- src/main/java/com/study/library/service/BookService.java
+++ src/main/java/com/study/library/service/BookService.java
@@ -1,9 +1,6 @@
 package com.study.library.service;
 
-import com.study.library.dto.RegisterBookReqDto;
-import com.study.library.dto.SearchBookCountRespDto;
-import com.study.library.dto.SearchBookReqDto;
-import com.study.library.dto.SearchBooksRespDto;
+import com.study.library.dto.*;
 import com.study.library.entity.Book;
 import com.study.library.repository.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,5 +49,15 @@ public class BookService {
                 .totalCount(bookCount)
                 .maxPageNumber(maxPageNumber)
                 .build();
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void deleteBooks(List<Integer> bookIds) {
+        bookMapper.deleteBooksByBookIds(bookIds);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void updateBook(UpdateBookReqDto updateBookReqDto) {
+        bookMapper.updateBookByBookId(updateBookReqDto.toEntity());
     }
 }
